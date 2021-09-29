bff12ce85d089cd1bb8509bf9bb242cc373370a0
Grzegorz <g.juzba@gmail.com>
2021-09-29 18:30:40 +0200

    add slider function

--- main.js
+++ main.js
@@ -8,3 +8,24 @@ burger.addEventListener("click", function(){
 menu.addEventListener("click", function(){
     menu.classList.toggle("active");
 })
+
+const slideList=[
+{text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod totam sit magni quos magnam?"},
+{text:"Fugiat, repellendus aspernatur quo vel aut veritatis hic consectetur laboriosam."},
+{text:"Perspiciatis alias culpa molestiae qui, ipsum sit? Expedita excepturi exercitationem officia dolor."}
+]
+
+const slide= document.querySelector('.slide')
+const time = 5000;
+let active = 0;
+
+
+const changeSlide = () => {
+    active++;
+    if (active === slideList.length) {
+     active = 0;
+    }
+    slide.textContent = slideList[active].text;
+   }
+   let indexInterval = setInterval(changeSlide, time)
+
